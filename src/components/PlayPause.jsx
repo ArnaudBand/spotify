fa64362937abe53f904979d5d0794aa30de5478a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-04-12 09:21:22 +0000

    Add functionality of play music

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,21 @@
-const PlayPause = () => (
-  <div>Loader</div>
+import { BsPause, BsFillPlayCircleFill } from 'react-icons/bs';
+import SongBar from './SongBar';
+
+const PlayPause = ({ isPlaying, activeSong, handlePlay, handlePause, song }) => (
+  isPlaying && activeSong?.title === SongBar.title ?
+    (
+      <BsPause
+        size={35}
+        className='text-gray-300'
+        onClick={handlePause}
+      />
+    ) : (
+      <BsFillPlayCircleFill
+        size={35}
+        className='text-gray-300'
+        onClick={handlePlay}
+      />
+    )
 );
 
 export default PlayPause;
