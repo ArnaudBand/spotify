15ec743e0d80d2e75fd2d0363b0eb6d80340beaf
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-04-12 09:20:43 +0000

    Active the play button and pause

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,23 +1,31 @@
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
-import { playPause, setActiveSongs } from '../redux/features/playerSlice';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, i }) => {
+const SongCard = ({ song, activeSong, isPlaying, data, i }) => {
 
-   const activeSong = 'Test';
+  const dispatch = useDispatch();
 
-   const handlePauseClick = () => {}
+   const handlePauseClick = () => {
+    dispatch(playPause(false));
+   }
 
-   const handlePlayClick = () => {}
+   const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+   }
+
   return (
   <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
     <div className="relative w-full h-56 group">
       <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
         <PlayPause 
-          song={song}
           handlePause={handlePauseClick}
           handlePlay={handlePlayClick}
+          song={song}
+          isPlaying={isPlaying}
+          activeSong={activeSong}
         />
       </div>
       <img src={song.images?.coverart} alt="song_img" />
