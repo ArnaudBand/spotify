eb7b1526f64b95b6d564ea66a46ab24fcb8e8cf3
ArnaudBand <bandonkeyea@gmail.com>
2023-11-25 12:37:45 +0300

    Fix onclick function and add playlist icon

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { TbPlaylist } from 'react-icons/tb'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 const Library = () => {
-  const onClick = () => {
-    console.log('click')
+  const onclick = () => {
+    // TODO: Add playlist
   }
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
-        <div className="inline-flex items-center gap-x-2"></div>
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist className="w-6 h-6 text-neutral-400" />
+          <span className='text-neutral-400 font-medium text-md'>Your Library</span>
+        </div>
+        <AiOutlinePlus className="w-6 h-6 text-neutral-400 hover:text-white cursor-pointer transition"
+        onClick={onclick} 
+        />
+      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        List of Songs!
       </div>
     </div>
   )
