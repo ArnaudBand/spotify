9cb28ac15e6b82258c4ce700232edadaaff82d49
ArnaudBand <bandonkeyea@gmail.com>
2024-01-18 12:10:28 +0300

    Refactor layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,32 @@
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
 
-import Sidebar from '@/components/Sidebar'
-import SupabaseProvider from '@/providers/SupabaseProvider'
+import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
 
-const inter = Figtree({ subsets: ['latin'] })
+const inter = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotify',
-  description: 'Listen to music on Spotify.',
-}
+  title: "Spotify",
+  description: "Listen to music on Spotify.",
+};
 
-export default function RootLayout ({
+export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <SupabaseProvider>
-        <Sidebar>
-          {children}
-        </Sidebar>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
-  )
+  );
 }
