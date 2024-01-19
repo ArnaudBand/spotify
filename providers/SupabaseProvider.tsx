dcc1609a429a20ba1ed13ab785d44fd59aeae3ca
ArnaudBand <bandonkeyea@gmail.com>
2024-01-20 01:59:00 +0300

    Update dependencies for @supabase/auth-helpers-nextjs and @supabase/auth-helpers-react

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -2,18 +2,18 @@
 
 
 import React, { useState } from "react";
-import { Database } from "@/types_db"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { Database } from "@/types_db";
 
 interface SupabaseProviderProps {
   children: React.ReactNode;
 }
 
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
-  const [supabaseClient] = useState<any>(() => {
+  const [supabaseClient] = useState(() => 
     createClientComponentClient<Database>()
-  });
+  );
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient}>
