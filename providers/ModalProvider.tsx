50db309787a28786f9722558963df56128bc3a32
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-20 06:03:15 +0000

    Refactor ModalProvider component

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,23 +1,29 @@
-'use client';
+"use client";
 
-import Modal from '@/components/Modal';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import Modal from "@/components/Modal";
 
 const ModalProvider = () => {
-
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsMounted(true)
-  }, [])
+    setIsMounted(true);
+  }, []);
 
-  if(!isMounted) return null;
+  if (!isMounted) return null;
 
   return (
     <div>
-      <Modal />
+      <Modal
+        title="Test Modal"
+        description="Test Description"
+        isOpen
+        onChange={() => {}}
+      >
+        Test Children
+      </Modal>
     </div>
   );
-}
+};
 
 export default ModalProvider;
