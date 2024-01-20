3f5930dda9ebada816416e50901118c2a3b55d26
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-20 08:01:39 +0000

    Add useAuthModal hook to Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/navigation";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
+
+import useAuthModal from "@/hooks/useAuthModal";
 import Button from "./Button";
 
 interface HeaderProps {
@@ -16,6 +18,8 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({
   children, className
 }) => {
+
+  const authModal = useAuthModal()
   const router = useRouter()
 
   const handleLogout = () => { };
@@ -48,14 +52,14 @@ const Header: React.FC<HeaderProps> = ({
           <>
             <div className="">
               <Button
-              onClick={() => {}}
+              onClick={authModal.onOpen}
               className="bg-transparent text-neutral-300 font-medium">
                 Sign Up
               </Button>
             </div>
             <div className="">
               <Button
-              onClick={() => {}}
+              onClick={authModal.onOpen}
               className="bg-white px-6 py-2">
                 Log In
               </Button>
