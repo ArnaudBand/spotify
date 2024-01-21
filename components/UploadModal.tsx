9a034fd9022ae4c935cefc3dbf0ad829833696ec
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-21 20:17:29 +0000

    Add UploadModal component

---
+++ components/UploadModal.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      uploadModal.onClose();
+    }
+  };
+  return (
+    <Modal
+      title="Upload modal title"
+      description="Upload modal description"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      <p>Upload modal content</p>
+    </Modal>
+  );
+};
+
+export default UploadModal;
