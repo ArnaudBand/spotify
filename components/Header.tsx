99a37337453eb3ed8bfd1b416ff6b36e7085eca7
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-21 19:26:16 +0000

    Refactor Header component and add logout functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,75 +1,110 @@
 "use client";
 
-import React from 'react';
+import React from "react";
 import { twMerge } from "tailwind-merge";
 import { useRouter } from "next/navigation";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
-import { HiHome } from 'react-icons/hi';
-import { BiSearch } from 'react-icons/bi';
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { FaUserAlt } from "react-icons/fa";
+import toast from "react-hot-toast";
 
 import useAuthModal from "@/hooks/useAuthModal";
 import Button from "./Button";
+import { useUser } from "@/hooks/useUser";
 
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-  children, className
-}) => {
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const authModal = useAuthModal();
+  const router = useRouter();
 
-  const authModal = useAuthModal()
-  const router = useRouter()
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
 
-  const handleLogout = () => { };
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    // TODO: Reset any playing songs
+    router.refresh();
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success("Logged out successfully");
+    }
+  };
 
   return (
-    <div className={
-      twMerge(
+    <div
+      className={twMerge(
         "h-fit bg-gradient-to-b from-green-300 p-6",
         className
-      )
-    }>
+      )}
+    >
       <div className="flex justify-between items-center mb-4 w-full">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button onClick={() => router.back()} className='rounded-full bg-black flex items-center justify-center hover:opacity-80 transition'>
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-80 transition"
+          >
             <RxCaretLeft className="text-white" size={35} />
           </button>
-          <button onClick={() => router.forward()} className='rounded-full bg-black flex items-center justify-center hover:opacity-80 transition'>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-80 transition"
+          >
             <RxCaretRight className="text-white" size={35} />
           </button>
         </div>
         <div className="flex md:hidden gap-x-2 items-center">
-          <button className='rounded-full bg-white p-2 flex items-center hover:opacity-75 transition'>
+          <button className="rounded-full bg-white p-2 flex items-center hover:opacity-75 transition">
             <HiHome className="text-black" size={20} />
           </button>
-          <button className='rounded-full bg-white p-2 flex items-center hover:opacity-75 transition'>
+          <button className="rounded-full bg-white p-2 flex items-center hover:opacity-75 transition">
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
-          <>
-            <div className="">
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
               <Button
-              onClick={authModal.onOpen}
-              className="bg-transparent text-neutral-300 font-medium">
-                Sign Up
+                onClick={() => router.push("/account")}
+                className="bg-white rounded-full p-2"
+              >
+                <FaUserAlt />
               </Button>
             </div>
-            <div className="">
-              <Button
-              onClick={authModal.onOpen}
-              className="bg-white px-6 py-2">
-                Log In
-              </Button>
-            </div>
-          </>
+          ) : (
+            <>
+              <div className="">
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-transparent text-neutral-300 font-medium"
+                >
+                  Sign Up
+                </Button>
+              </div>
+              <div className="">
+                <Button
+                  onClick={authModal.onOpen}
+                  className="bg-white px-6 py-2"
+                >
+                  Log In
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
