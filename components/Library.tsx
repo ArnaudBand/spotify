be3b46a3bb425e142b53bf1edf89abba3ccc070c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-21 20:17:57 +0000

    Refactor Library component and add authentication and upload modals

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import { TbPlaylist } from 'react-icons/tb'
-import { AiOutlinePlus } from 'react-icons/ai'
+"use client";
+
+import React from "react";
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
-  const onclick = () => {
-    // TODO: Add playlist
-  }
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
+  const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+    // TODO:Check for subscription
+    return uploadModal.onOpen();
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="w-6 h-6 text-neutral-400" />
-          <span className='text-neutral-400 font-medium text-md'>Your Library</span>
+          <span className="text-neutral-400 font-medium text-md">
+            Your Library
+          </span>
         </div>
-        <AiOutlinePlus className="w-6 h-6 text-neutral-400 hover:text-white cursor-pointer transition"
-        onClick={onclick} 
+        <AiOutlinePlus
+          className="w-6 h-6 text-neutral-400 hover:text-white cursor-pointer transition"
+          onClick={onClick}
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of Songs!
-      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
     </div>
-  )
-}
+  );
+};
 
 export default Library;
