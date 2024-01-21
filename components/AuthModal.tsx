5f57bb066740846c25f874fdd6a651fa9d00ae00
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-01-21 19:26:02 +0000

    Refactor AuthModal component

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
-import { useSupabaseClient, useSessionContext } from "@supabase/auth-helpers-react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import {
+  useSupabaseClient,
+  useSessionContext,
+} from "@supabase/auth-helpers-react";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 import Modal from "./Modal";
-import useAuthModal from '@/hooks/useAuthModal';
+import useAuthModal from "@/hooks/useAuthModal";
 
 const AuthModal = () => {
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
   const { session } = useSessionContext();
-  const {isOpen, onClose} = useAuthModal();
+  const { isOpen, onClose } = useAuthModal();
 
-  const onChange = (open: boolean) => {
-    if(!open) {
+  useEffect(() => {
+    if (session) {
+      router.refresh();
       onClose();
     }
-  }
+  }, [session, router, onClose]);
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
 
   return (
     <Modal
@@ -37,11 +48,11 @@ const AuthModal = () => {
           variables: {
             default: {
               colors: {
-                brand: '#404040',
-                brandAccent: '#22c55e'
-              }
-            }
-          }
+                brand: "#404040",
+                brandAccent: "#22c55e",
+              },
+            },
+          },
         }}
       />
     </Modal>
