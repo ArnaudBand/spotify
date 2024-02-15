82beed786340d8fa142e9cb4085be2c7cb8d44e9
ArnaudBand <bandonkeyea@gmail.com>
2024-02-15 16:14:25 +0300

    Display the like songs

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,13 +1,52 @@
-import {Song} from "@/types";
+"use client";
 
+import React, {useEffect} from "react";
+import {useRouter} from "next/navigation";
+
+import {Song} from "@/types";
+import {useUser} from "@/hooks/useUser";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
 
 interface LikedContentProps {
-  songs: Song[]
+  songs: Song[];
 }
 const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+
+  const {isLoading, user} = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/')
+    }
+  }, [isLoading, user, router]);
+
+  if (songs.length === 0) {
+    return (
+      <p className="
+        flex
+        flex-col
+        gap-y-2
+        w-full
+        px-6
+        text-neutral-400
+      ">
+        No liked songs.
+      </p>
+    )
+  }
+
   return (
-    <div>
-      Liked Content
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song) => (
+        <div className={'flex items-center gap-x-4 w-full'} key={song.id}>
+          <div className={"flex-1"}>
+            <MediaItem data={song} onClick={() => {}} />
+          </div>
+          <LikeButton  songId={song.id} />
+        </div>
+      ))}
     </div>
   )
 }
