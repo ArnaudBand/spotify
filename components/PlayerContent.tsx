c57e6a318f3d311a562ca3301618a8f65838fb7e
ArnaudBand <bandonkeyea@gmail.com>
2024-02-20 23:47:33 +0300

    Organize the code

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -104,84 +104,27 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl}) => {
         </div>
       </div>
 
-      <div
-        className="
-            flex 
-            md:hidden 
-            col-auto 
-            w-full 
-            justify-end 
-            items-center
-          "
-      >
-        <div
-          onClick={handlePlay}
-          className="
-              h-10
-              w-10
-              flex 
-              items-center 
-              justify-center 
-              rounded-full 
-              bg-white 
-              p-1 
-              cursor-pointer
-            "
-        >
+      <div className="flex md:hidden col-auto w-full justify-end items-center">
+        <div onClick={handlePlay} className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer">
           <Icon size={30} className="text-black" />
         </div>
       </div>
 
-      <div
-        className="
-            hidden
-            h-full
-            md:flex 
-            justify-center 
-            items-center 
-            w-full 
-            max-w-[722px] 
-            gap-x-6
-          "
-      >
+      <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
         <AiFillStepBackward
           onClick={onPlayPrevious}
           size={30}
-          className="
-              text-neutral-400 
-              cursor-pointer 
-              hover:text-white 
-              transition
-            "
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
-        <div
-          onClick={handlePlay}
-          className="
-              flex 
-              items-center 
-              justify-center
-              h-10
-              w-10 
-              rounded-full 
-              bg-white 
-              p-1 
-              cursor-pointer
-            "
-        >
+        <div onClick={handlePlay} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
           onClick={onPlayNext}
           size={30}
-          className="
-              text-neutral-400 
-              cursor-pointer 
-              hover:text-white 
-              transition
-            "
+          className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-
       <div className="hidden md:flex w-full justify-end pr-2">
         <div className="flex items-center gap-x-2 w-[120px]">
           <VolumeIcon
@@ -195,7 +138,6 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl}) => {
           />
         </div>
       </div>
-
     </div>
   );
 }
